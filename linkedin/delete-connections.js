3c6f09116d8f41d96e7f2ff0c4ee2f8d58c80835
Vicky <vicky@vickylai.com>
2018-06-05 17:08:59 -0400

    Sleep function moved within anonymous function wrapper

--- linkedin/delete-connections.js
+++ linkedin/delete-connections.js
@@ -1,19 +1,20 @@
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-async function removeConnections() {
-    for (var i = 0; i < 10; i++){
-        document.getElementsByClassName("search-result__result-link")[0].click();
-        await sleep(4000);
-        document.getElementsByClassName("pv-s-profile-actions__overflow-toggle")[0].click();
-        await sleep(1500);
-        document.getElementsByClassName("pv-s-profile-actions--disconnect")[0].click();
-        await sleep(1000);
-        document.getElementsByClassName("pv-s-profile-actions--unfollow")[0].click();
-        await sleep(1000);
-        window.history.back();
-        await sleep(2000);
+javascript: (function () {
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
     }
-}
 
+    async function removeConnections() {
+        for (var i = 0; i < 10; i++) {
+            document.getElementsByClassName("search-result__result-link")[0].click();
+            await sleep(4000);
+            document.getElementsByClassName("pv-s-profile-actions__overflow-toggle")[0].click();
+            await sleep(1500);
+            document.getElementsByClassName("pv-s-profile-actions--disconnect")[0].click();
+            await sleep(1000);
+            document.getElementsByClassName("pv-s-profile-actions--unfollow")[0].click();
+            await sleep(1000);
+            window.history.back();
+            await sleep(2000);
+        }
+    }
+}());
--- linkedin/delete-posts.js
+++ linkedin/delete-posts.js
@@ -1,15 +1,18 @@
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-async function removePosts() {
-    while (document.getElementsByClassName("control-panel-trigger").length > 1) {
-        document.getElementsByClassName("control-panel-trigger")[0].click();
-        await sleep(1000);
-        document.getElementsByClassName("option-button__delete")[0].click();
-        await sleep(1000);
-        document.getElementsByClassName("feed-base-decision-modal__confirm-button")[0].click();
-        await sleep(2000);
-        window.scrollBy(0, 20);
+javascript: (function () {
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
     }
-}removePosts();
+
+    async function removePosts() {
+        while (document.getElementsByClassName("control-panel-trigger").length > 1) {
+            document.getElementsByClassName("control-panel-trigger")[0].click();
+            await sleep(1000);
+            document.getElementsByClassName("option-button__delete")[0].click();
+            await sleep(1000);
+            document.getElementsByClassName("feed-base-decision-modal__confirm-button")[0].click();
+            await sleep(2000);
+            window.scrollBy(0, 20);
+        }
+    }
+    removePosts();
+}());
