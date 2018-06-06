5b75c1d8ea04fbeec5f843656e1f6476d6ae7040
Adam Drake <adrake@gmail.com>
2018-06-06 10:41:16 -0400

    Update LinkedIn bookmarklets

--- linkedin/delete-messages.js
+++ linkedin/delete-messages.js
@@ -2,15 +2,15 @@ javascript: (function () {
     function sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
-
-    async function demo() {
-        for (var i = 1; i < 100; i++) {
-            await sleep(1000);
+    async function deleteMessages() {
+        while (document.getElementbyClassName("msg-thread__topcard-btn").length > 1) {
             document.getElementsByClassName("msg-thread__topcard-btn")[1].click();
-            await sleep(1000)
+            await sleep(1000);
             document.getElementsByClassName("msg-topcard__full-width-action-button delete")[0].click();
-            await sleep(1000)
+            await sleep(1000);
             document.getElementsByClassName("msg-modal__button confirm-delete-btn")[0].click();
+            await sleep(1000);
         }
     }
+    deleseMessages();
 }());
