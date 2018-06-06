6d94868494f9d7ce4e161919fd54c41b0118866d
Vicky <vicky@vickylai.com>
2018-06-06 19:56:02 -0400

    Made Cancel button on prompt work.

--- twitter/delete-tweets.js
+++ twitter/delete-tweets.js
@@ -1,16 +1,14 @@
 javascript: (function () {
     function sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
-    }
-
+    };
     var now = new Date;
-    var rawTweets = document.querySelectorAll("div.js-original-tweet div.content");
+    var rawTweets = document.querySelectorAll('div.js-original-tweet div.content');
     tweets = Array.from(rawTweets).map(t => ({
-        time: t.querySelector("div.stream-item-header small a span.js-short-timestamp").getAttribute("data-time-ms"),
-        delete: t.querySelector("div.stream-item-header div.js-more-ProfileTweet-actions div.dropdown div.dropdown-menu ul li.js-actionDelete"),
-        unretweet: t.querySelector("div.stream-item-footer div.js-actions div.ProfileTweet-action--retweet button.js-actionRetweet")
+        time: t.querySelector('div.stream-item-header small a span.js-short-timestamp').getAttribute('data-time-ms'),
+        delete: t.querySelector('div.stream-item-header div.js-more-ProfileTweet-actions div.dropdown div.dropdown-menu ul li.js-actionDelete'),
+        unretweet: t.querySelector('div.stream-item-footer div.js-actions div.ProfileTweet-action--retweet button.js-actionRetweet')
     }));
-
     async function removeTweets(max) {
         for (let i = 0; i < tweets.length; i++) {
             let stamp = new Date(parseInt(tweets[i].time));
@@ -19,7 +17,7 @@ javascript: (function () {
                 if (tweets[i].delete != null) {
                     tweets[i].delete.click();
                     await sleep(1000);
-                    document.getElementsByClassName("delete-action")[0].click();
+                    document.getElementsByClassName('delete-action')[0].click();
                     await sleep(1000);
                 } else if (tweets[i].delete == null) {
                     tweets[i].unretweet.click();
@@ -28,5 +26,10 @@ javascript: (function () {
             }
         }
     }
-    removeTweets(prompt('Delete tweets older than __ hours:', '24'))
+    var hours = prompt('Delete tweets older than __ hours:', '24')
+    if (hours == null) {
+        return
+    } else {
+        removeTweets(hours);
+    }
 }());
