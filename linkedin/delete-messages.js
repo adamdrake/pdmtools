6ae3735b731d7189464db69f9d2de97e0de4e80a
Vicky <vicky@vickylai.com>
2018-06-06 21:43:57 -0400

    Removed spacing for bookmarklets prep.

--- linkedin/delete-messages.js
+++ linkedin/delete-messages.js
@@ -1,16 +1,1 @@
-javascript: (function () {
-    function sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
-    }
-    async function deleteMessages() {
-        while (document.getElementsByClassName("msg-thread__topcard-btn").length > 1) {
-            document.getElementsByClassName("msg-thread__topcard-btn")[1].click();
-            await sleep(1000);
-            document.getElementsByClassName("msg-topcard__full-width-action-button delete")[0].click();
-            await sleep(1000);
-            document.getElementsByClassName("msg-modal__button confirm-delete-btn")[0].click();
-            await sleep(1000);
-        }
-    }
-    deleteMessages();
-}());
+javascript: (function () {function sleep(ms) {return new Promise(resolve => setTimeout(resolve, ms));}async function deleteMessages() {while (document.getElementsByClassName('msg-thread__topcard-btn').length > 1) {document.getElementsByClassName('msg-thread__topcard-btn')[1].click();await sleep(1000);document.getElementsByClassName('msg-topcard__full-width-action-button delete')[0].click();await sleep(1000);document.getElementsByClassName('msg-modal__button confirm-delete-btn')[0].click();await sleep(1000);}}deleteMessages();}());
--- linkedin/delete-posts.js
+++ linkedin/delete-posts.js
@@ -1,17 +1,1 @@
-javascript: (function () {
-    function sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
-    }
-    async function removePosts() {
-        while (document.getElementsByClassName("control-panel-trigger").length > 0) {
-            document.getElementsByClassName("control-panel-trigger")[0].click();
-            await sleep(1000);
-            document.getElementsByClassName("option-button__delete")[0].click();
-            await sleep(1000);
-            document.getElementsByClassName("feed-shared-decision-modal__confirm-button")[0].click();
-            await sleep(2000);
-            window.scrollBy(0, 20);
-        }
-    }
-    removePosts();
-}());
+javascript: (function () {function sleep(ms) {return new Promise(resolve => setTimeout(resolve, ms));}async function removePosts() {while (document.getElementsByClassName('control-panel-trigger').length > 0) {document.getElementsByClassName('control-panel-trigger')[0].click();await sleep(1000);document.getElementsByClassName('option-button__delete')[0].click();await sleep(1000);document.getElementsByClassName('feed-shared-decision-modal__confirm-button')[0].click();await sleep(2000);window.scrollBy(0, 20);}}removePosts();}());
